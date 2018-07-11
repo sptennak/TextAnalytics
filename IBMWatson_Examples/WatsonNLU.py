de5ab2f15373c369d52d4aa7ac456a461372877a
sptennak <sptennak@users.noreply.github.com>
2018-07-11 00:27:22 -0400

    Update

---
+++ IBMWatson_Examples/WatsonNLU.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 18 22:15:35 2018
+
+@author: stennakoon
+"""
+
+from watson_developer_cloud import NaturalLanguageUnderstandingV1, WatsonException, WatsonApiException
+from watson_developer_cloud.natural_language_understanding_v1 import Features, EntitiesOptions, KeywordsOptions, RelationsOptions
+import pandas as pd
+import numpy as np
+from timeit import default_timer as timer
+import multiprocessing
+import sys
+
+###############################################################################
+def IAM_Auth(APIKey, Version):
+	ServiceAuthentication = NaturalLanguageUnderstandingV1(
+		version= Version,
+		iam_api_key= APIKey
+	)
+	ServiceAuthentication.set_url('https://gateway-fra.watsonplatform.net/natural-language-understanding/api')
+    #To prevent IBM from accessing user input and Watson responses... https://www.ibm.com/watson/developercloud/conversation/api/v1/python.html?python#data-collection
+    ServiceAuthentication.set_default_headers({'x-watson-learning-opt-out': "true"}) 
+	return ServiceAuthentication
+
+def Basic_Auth(UserName, Password, Version)
+	ServiceAuthentication = NaturalLanguageUnderstandingV1(
+		version= Version,
+		username= UserName,
+		password= Password
+	)
+	ServiceAuthentication.set_url('https://gateway-fra.watsonplatform.net/natural-language-understanding/api')
+    #To prevent IBM from accessing user input and Watson responses... https://www.ibm.com/watson/developercloud/conversation/api/v1/python.html?python#data-collection
+    ServiceAuthentication.set_default_headers({'x-watson-learning-opt-out': "true"}) 
+	return ServiceAuthentication
+###############################################################################
+
+def TextNLU(ServiceAuthentication, TextID, Text, ModelID=None, Emotion=False, Sentiment=False, Mentions =False, EntityLimit=50, TextLimit=50000, ReturnText=True):
+	Notes = ''	
+    try:
+        Response = ServiceAuthentication.analyze(
+            text=Text,
+            features=Features(
+                relations=RelationsOptions(
+                  model = ModelID,
+                ),
+                entities=EntitiesOptions(
+                  emotion=Emotion,
+                  sentiment=Sentiment,
+				  mentions=Mentions,
+                  model = ModelID,  
+                  limit=EntityLimit				  
+                ),     
+            ),
+			limit_text_characters = TextLimit, #https://console.bluemix.net/docs/services/natural-language-understanding/usage-limits.html#usage-limits
+            return_analyzed_text=ReturnText    
+        )
+		Notes='RECIEVED'
+    except WatsonException:  
+		Notes = str(WatsonException)
+		print('Watson NLU: WatsonException [{}]'.format(Response))
+	except WatsonApiException:
+		Notes = str(WatsonApiException)
+		print('Watson NLU: WatsonApiException [{}]'.format(Response))
+	except WatsonInvalidArgument:
+		Notes = str(WatsonInvalidArgument)
+		print('Watson NLU: WatsonInvalidArgument [{}]'.format(Response))	
+	except:
+		Notes = sys.exc_info()[0]
+		print('Watson NLU: Unexpected error [{}]'.format(Response))
+	Notes = 'NLU:'+Notes
+	
+	# Process Response Header
+	WatsonResponseHeader = pd.DataFrame({'TextID':[TextID]})
+    try:      	 
+        WatsonResponseHeader['Language'] = Response['language']
+        WatsonResponseHeader['CharacterCount'] = Response['usage']['text_characters'] #Number of characters processed
+        WatsonResponseHeader['NLUUnits'] = Response['usage']['text_units']	#Number of characters processed	
+		WatsonResponseHeader['Features'] = Response['usage']['features'] #Number of features used, such as entities, sentiment, etc.
+		WatsonResponseHeader['EntitiesCount'] = len(Response['entities'])
+        WatsonResponseHeader['AnalyzedText'] = Response['analyzed_text']		
+    except:
+		Notes= Notes+ '\tHEADER:' + sys.exc_info()[0]		
+	
+	
+	# Process Response Details	
+    try:
+        if len(Response['entities']) != 0:
+            WatsonResponseDetail = pd.DataFrame(Response['entities'])
+			WatsonResponseDetail.insert(0, 'EmailID', EmailID)
+        else:
+            raise Exception('NO ENTITIES FOUND')
+    except:
+        Notes= Notes+ '\DETAIL:' + sys.exc_info()[0]
+		WatsonResponseDetail = pd.DataFrame()
+		
+	WatsonResponseHeader['Notes'] = Notes	
+	
+	return WatsonResponseHeader, WatsonResponseDetail
+	
--- TextProcessing/TextProcessing.py
+++ TextProcessing/TextProcessing.py
@@ -30,11 +30,11 @@ def RemoveQuotedPrintableEncoding(Text):
 	#https://www.w3.org/Protocols/rfc1341/5_Content-Transfer-Encoding.html
 	#https://stackoverflow.com/questions/25710599/content-transfer-encoding-7bit-or-8-bit
     QPCTE = {
-            '= '    :' ', #Soft line break (ignore).
-            '==09'  :' ', #Soft line break (ignore).
-            '==20'  :' ', #Soft line break (ignore).  
-            '=\t'  :' ', #Soft line break (ignore). 
-            '=\n'  :' ', #Soft line break (ignore). 			
+            '= '    :'', #Soft line break (ignore).
+            '==09'  :'', #Soft line break (ignore).
+            '==20'  :'', #Soft line break (ignore).  
+            '=\t'  :'', #Soft line break (ignore). 
+            '=\n'  :'', #Soft line break (ignore). 			
             '=09'   :'\t',  
             '=0A'   :'\n',
             '=0C'   :'\f', 
@@ -61,9 +61,44 @@ def RemoveQuotedPrintableEncoding(Text):
             '=3C'   :'<', 
             '=3D'   :'=',
             '=3E'   :'>', 
-            '=3F'   :'?', 
-            '=A9'   :u'\xA9', 
-            '=AE'   :u'\xAE'
+            '=3F'   :'?',
+            '=85'   :'...',
+            '=91'   :"'",
+            '=92'   :"'",
+            '=93'   :'"',
+            '=94'   :'"',
+            '=95'   :u'\x95',
+            '=96'   :'-',
+            '=97'   :'--',
+            '=98'   :'~',
+            '=99'   :u'\x99',  #TM
+            '=A9'   :u'\xA9',  #Copyright
+            '=AE'   :u'\xAE',   #Registered
+            '=E0'   :u'\xE0',
+            '=E1'   :u'\xE1',
+            '=E2'   :u'\xE2',
+            '=E3'   :u'\xE3',   
+            '=E4'   :u'\xE4', 
+            '=E8'   :u'\xE8', 
+            '=E9'   :u'\xE9', 
+            '=EA'   :u'\xEA', 
+            '=EB'   :u'\xEB',
+            '=EC'   :u'\xEC',
+            '=ED'   :u'\xED',
+            '=EE'   :u'\xEE',
+            '=F1'   :u'\xF1',
+            '=F2'   :u'\xF2',
+            '=F3'   :u'\xF3',   
+            '=F4'   :u'\xF4',        
+            '=F5'   :u'\xF5',  
+            '=F6'   :u'\xF6',
+            '=F7'   :u'\xF7',
+            '=F8'   :u'\xF8',   
+            '=F9'   :u'\xF9',        
+            '=FA'   :u'\xFA', 
+            '=FB'   :u'\xFB', 
+            '=FC'   :u'\xFC', 
+            '=FD'   :u'\xFD', 
             }
 
     for (pattern,replacement) in QPCTE.items():
@@ -100,15 +135,15 @@ def RemoveHTMLTagsEntities(Text):
             '&nb sp;' :' ',
             '&nbs p;' :' ',
             '&quot;'  :'"',
-            'cent;'   :u'\xA2'
-            '&pound;' :u'\xA3'
+            'cent;'   :u'\xA2',
+            '&pound;' :u'\xA3',
             '&copy;'  :u'\xA9',
             '&reg;'   :u'\xAE',
-            '&plusmn;':u'\xB1'
-            '&frac14;':u'\xBC'
-            '&frac12;':u'\xBD'
-            '&frac34;':u'\xBE'
-            '&times;' :u'\xD7'
+            '&plusmn;':u'\xB1',
+            '&frac14;':u'\xBC',
+            '&frac12;':u'\xBD',
+            '&frac34;':u'\xBE',
+            '&times;' :u'\xD7',
             '&prime;' :u'\x2032',
             '&Prime'  :u'\x2033',
             '&lowast;':u'\x2217',
@@ -122,3 +157,9 @@ def RemoveHTMLTagsEntities(Text):
 
     return Text
         
+def RemoveConsecutiveWhiteSpace(Text):
+    Text=re.sub('[\x20][\x20]+' , ' ', Text)
+    Text=re.sub('[\n][\n]+' , '\n', Text)
+    Text=re.sub('[\t][\t]+' , '\t', Text)
+    Text=re.sub('[\r][\r]+' , '\r', Text)
+    return Text
